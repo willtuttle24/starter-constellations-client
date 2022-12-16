eb30b3966e9218b890863a21889625c578c9d4ae
Will Tuttle <willtuttle24@gmail.com>
2022-12-16 15:43:24 -0600

    1

--- src/common/Header.js
+++ src/common/Header.js
@@ -5,7 +5,7 @@ function Header ({ constellations, setConstellations }) {
   return (
     <div className="jumbotron jumbotron-fluid bg-info text-white rounded-0 mb-0">
       <div className="container">
-        <h1 className="display-4">Explore Constellations</h1>
+        <h1 className="display-4">I love Constellations</h1>
         <p className="lead">Explore different constellations in the sky!</p>
         <hr />
         <Search
